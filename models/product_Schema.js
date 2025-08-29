7280ce5fa1e0bb5bee6e357720595649c7aa8f56
Muneeb UR Rehman <118824843+MUNEEBAZAM96@users.noreply.github.com>
2025-08-29 09:59:57 +0500

    Create product_Schema.js

---
+++ models/product_Schema.js
@@ -0,0 +1,43 @@
+const mongoos = require('mongoose');
+
+
+
+const Product_Schema = mongoos.Schema({
+    
+    name : {
+        type: String,
+        require: [true,'please enter the name , without name product donot exist']
+        
+    },
+
+    Quantity : {
+           type : Number,
+           require: [true,'please enter the quantity , without quantity product donot exist'],
+           default:0
+
+
+    },
+
+
+
+    price:{
+        type: Number,
+        require: [true,'please enter the price , without price product donot exist'],
+        default:0
+
+    } ,
+    image:{
+          type: String,
+          require : true,
+          default: ''
+    }
+},
+    {
+
+        Timestamp: true
+
+    }
+);
+
+
+const Product = mongoos.model('Product', Product_Schema);
